b0aab11a053341688e7e51eb571bee8eb6a23153
nilofarmujawar1118 <nilofarmujawar1118@gmail.com>
2022-03-24 20:37:22 +0530

    Added: Added: Ability to load the census information of Indian State from the CSV file functionality added UC1.

--- src/main/java/com/day29/CensusAnalyser.java
+++ src/main/java/com/day29/CensusAnalyser.java
@@ -1,4 +1,12 @@
 package com.day29;
+/**
+ * UC1 :- Ability for the analyser to load the Indian States Census Information from a csv file
+ *        - Create a StateCensusAnalyser Class to load  the State Census CSV Data
+ *        - Create CSVStateCensus Class to load the CSV Data
+ *        - Use Iterator to load the data
+ *        - Check with StateCensusAnalyser to ensure no of record matches.
+ *
+ */
 
 
 import com.opencsv.bean.CsvToBean;
